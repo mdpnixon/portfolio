b9f191b101de40d7dbb0e354398f2a1e320d4da0
mdpnixon <mdpnixon@gmail.com>
2021-05-10 16:25:26 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/api/emails', require('./routes/api/emails.js'));
-
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
